61dffef85cb703a5d7088639862fe444824dec80
lelagiorgadze99 <118673205+lelagiorgadze99@users.noreply.github.com>
2023-05-17 19:30:13 +0400

    create reset/custom

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ const pZero = document.getElementById("p-zero");
 const peopleDiv = document.getElementById("svg-input");
 const integer = document.getElementById("integer");
 const resetBtn = document.getElementById('resetBtn');
+const custom = document.getElementById('custom');
 let tip;
 let billValue;
 let peopleValue;
 let tipAmount;
 let total;
+let customValue;
 
 Array.from(percentageBtn).forEach((btn) => {
   btn.addEventListener("click", (event) => {
@@ -26,19 +28,32 @@ bill.addEventListener("input", (event) => {
   billValue = Number(event.target.value);
   console.log(billValue);
   calculate();
+  calculateCustom();
 });
 people.addEventListener("input", (event) => {
   peopleValue = Number(event.target.value);
   console.log(peopleValue);
   calculate();
+  calculateCustom();
   zero();
 });
+custom.addEventListener("input", (event)=> {
+  customValue = Number(event.target.value);
+  console.log(customValue);
+  calculateCustom();
+})
 resetBtn.addEventListener("click", reset);
 
 
 function calculate() {
   tipAmount = ((billValue * (tip / 100)) / peopleValue).toFixed(2);
   total = ((billValue + billValue * (tip / 100)) / peopleValue).toFixed(2);
+  tipResult.textContent = `$${tipAmount}`;
+  totalResult.textContent = `$${total}`;
+}
+function calculateCustom(){
+  tipAmount = ((billValue * (customValue / 100)) / peopleValue).toFixed(2);
+  total = ((billValue + billValue * (customValue / 100)) / peopleValue).toFixed(2);
   tipResult.textContent = `$${tipAmount}`;
   totalResult.textContent = `$${total}`;
 }
@@ -68,6 +83,8 @@ function reset(){
   tip = 0;
   tipResult.textContent = `$0.00`;
   totalResult.textContent = `$0.00`;
+  customValue = 0;
+  custom.value = 0;
 
 }
 
